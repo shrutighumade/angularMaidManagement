fa820e5cd8f5de06914170846ca59a50811de07a
shruti ghumade <shrutighumde@gmail.com>
2024-03-24 23:39:39 +0530

    routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/user-registration/components/login/login.component';
+import { RegistrationComponent } from './modules/user-registration/components/registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"login",
+        component:LoginComponent
+    },
+    {
+        path:"register",
+        component:RegistrationComponent
+    }
+];
